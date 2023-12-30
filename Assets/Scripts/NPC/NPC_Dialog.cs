917ecb701f90c9073c3615b1d57c63c69c82d842
Éder Pires <ederpbj@gmail.com>
2023-12-29 21:40:38 -0300

    6.4 Diálogo (Parte 3)

--- Assets/Scripts/NPC/NPC_Dialog.cs
+++ Assets/Scripts/NPC/NPC_Dialog.cs
@@ -1,18 +1,39 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC_Dialog : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float dialogueRange; //tamanho do circulo da colisão
+    public LayerMask playerLayer; //identifica a layer (camada)
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        //cria um circulo ao redor do NPC, identificando posição e layer
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer); 
+
+        if(hit != null)
+        {
+            Debug.Log("player na área de colisão!");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange); //desenha a esfera gizmo
     }
 }
